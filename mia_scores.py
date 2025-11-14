f2edbb240f1158d86a903fee8bb382f6ae9f60ee
Abinash Selvarajah <abinash.selvarajah@ruhr-uni-bochum.de>
2025-11-14 20:57:33 +0100

    Added min_k_probs and zlib_ratio

--- mia_scores.py
+++ mia_scores.py
@@ -1,12 +1,9 @@
 import torch
 import numpy as np
 from tqdm import tqdm
+import zlib
 
-def perplexity(sentence, model, tokenizer, gpu):
-    """
-    exp(loss)
-    """
-
+def raw_values(sentence, model, tokenizer, gpu):
     input_ids = torch.tensor(tokenizer.encode(sentence)).unsqueeze(0)
     input_ids = input_ids.to(gpu)
 
@@ -25,7 +22,38 @@ def perplexity(sentence, model, tokenizer, gpu):
         probability = probabilities[0, i, token_id].item()
         all_prob.append(probability)
 
-    return torch.exp(loss).item(), all_prob, loss.item()
+    return loss, all_prob, loss.item()
+
+def perplexity(loss):
+    """
+    exp(loss)
+    """    
+
+    return torch.exp(loss).item()
 
 
-# def k_min_probs(loss_list, k=0.05)
+def k_min_probs(all_prob, k=0.05):
+    """
+    Sum over k% lowest probability of tokens and then average them
+
+    Outputs average log-probabilites
+    """
+
+    sorted_prob = sorted(all_prob)
+    num_values = max(1, int(len(sorted_prob)*k))
+    k_min = sorted_prob[:num_values]
+    if sum(k_min)==0 or len(k_min)==0:
+        return 0
+    return sum(k_min)/len(k_min)
+
+# TO FIX
+def zlib_ratio(sentence):
+    # Encode the string into bytes (using UTF-8)
+    original_bytes = sentence.encode("utf-8")
+    
+    # Compress the bytes
+    compressed_bytes = zlib.compress(original_bytes)
+    
+    return len(compressed_bytes) / len(original_bytes)
+
+# TODO: Neighborhood based scores
