9e8d4bc19aa418a64a398c1d07010a939b561bde
Abinash Selvarajah <selvaah3@login001.cluster>
2025-11-27 13:56:48 +0100

    Test

--- aggregation.py
+++ aggregation.py
@@ -1,16 +1,14 @@
 import torch
-import numpy 
+import numpy as np
 from sklearn import svm
 from xgboost import XGBClassifier
 
-def xgboost(X_train,y_train,X_test,y_test):
-    
+def xgboost_aggregation(X_train, y_train, X_test, y_test):
     clf = XGBClassifier(tree_method="hist", early_stopping_rounds=5)
     clf.fit(X_train, y_train, eval_set=[(X_test, y_test)])
-
     return clf
 
 def svm_classifier(X_train, y_train):
     clf = svm.SVC()
-    clf.fit(X_train,y_train)
+    clf.fit(X_train, y_train)
     return clf
